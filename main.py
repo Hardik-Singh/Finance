b37ecbd2b7ece6a50bd8b6e00b276d58f1d43c87
Hardik-Singh <68667083+Hardik-Singh@users.noreply.github.com>
2024-04-02 10:38:05 -0500

    smlal changes
    small changes

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import yfinance as yf
 from datetime import datetime
 from fastapi import FastAPI
-
-companies = ['AMZN','GOOG','WMT','TSLA','META'] 
-
-tickers = yf.Tickers(companies)
-tickers_hist = tickers.history(period='max',start="2024-03-01", interval='1d')
-
-
-print(tickers_hist)
+import pandas as pd
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": tickers_hist}
+    def get_history():
+        companies = ['AMZN', 'GOOG', 'BTC-USD', 'TSLA', 'META']
+        df_list = []
+        for ticker in companies:
+            data = yf.download(ticker, start="2024-03-01",group_by="Ticker", period='2d')
+            data['ticker'] = ticker  # Add ticker column
+            df_list.append(data)
+
+        tickers_hist = pd.concat(df_list)
+        return tickers_hist
+
+    
+    history = get_history()
+    history.to_csv("data.csv")
+    
+    return {"message": history.to_string()}
