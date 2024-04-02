f8d3c8b6a9e887d810c856b5e221f7fa3a0aa6cf
Hardik-Singh <68667083+Hardik-Singh@users.noreply.github.com>
2024-04-01 22:13:49 -0500

    small changes
    small changes

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import yfinance as yf
 from datetime import datetime
+from fastapi import FastAPI
 
 companies = ['AMZN','GOOG','WMT','TSLA','META'] 
 
 tickers = yf.Tickers(companies)
-tickers_hist = tickers.history(period='max',start="2024-03-01", interval='1d',)
-tickers_hist
+tickers_hist = tickers.history(period='max',start="2024-03-01", interval='1d')
+
+
+print(tickers_hist)
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": tickers_hist}
